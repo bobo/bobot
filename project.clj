720952b6a0dbbd9577d9fb9f1f9c66b351bd8700
Mikael Sundberg <mikael.sundberg82@gmail.com>
2011-02-11 21:56:53 +0100

    project file

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject bobot "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [irclj "0.4.0-SNAPSHOT"]
+                 [enlive "1.0.0-SNAPSHOT"]               
+                 [midje "1.1-alpha-1"]
+                 [clj-http "0.1.2"]
+		 ]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :main bobot.core)
+  
