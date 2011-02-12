a7598701bac0af42864f0e514803ebe46ad3caa6
Mikael Sundberg <mikael.sundberg82@gmail.com>
2011-02-12 13:23:24 +0100

    currency converter

--- src/bobot/core.clj
+++ src/bobot/core.clj
@@ -12,43 +12,50 @@ representation of text."
   [text]
   (URLEncoder/encode text "UTF-8"))
 
+(defn make-url [address]
+    (if (.startsWith address "http")
+      (URL. address)
+      (URL. (str "http://" address))))
+
 (defn get-title [url]
   (-> url
       html-resource
       (select [:title])
       first :content first))
 
-(defn do-search [query]
+
+(defn google-it [query]
   (let [google (.openConnection (URL. (str "https://www.google.com/search?as_q=" query "&hl=sv")))]
     (.setRequestProperty google "User-agent" "irclj-bobot")
-    (let [ url (->
-                (select (html-resource
-                         (.getInputStream google)) [:a.l])
-                first
-                :attrs
-                :href
-                )]
-      (str (-> url
-               make-url
-               get-title)
-           " " url
-           ))))
+    (.getInputStream google)))
+
+(defn select-from [query tag]
+  (select (html-resource
+           (google-it (url-encode query))) tag))
+
+(defn do-search [query]
+  (let [ url (->
+              (select-from query [:a.l])
+              first :attrs :href)]
+    (str (-> url make-url get-title)
+         " " url)))
 
 (defn clean-html [page]
-  (flatten (map #(if (map? %) (:content %) %) page)))
+  (flatten (map #(if (map?  %) (clean-html (:content %)) %) page)))
 
 (defn get-info [query]
-  (let [google (.openConnection (URL. (str "https://www.google.com/search?as_q=" query "&hl=sv")))]
-    (.setRequestProperty google "User-agent" "irclj-bobot")
-    (apply str (->
-                (select (html-resource
-                         (.getInputStream google)) [:div.s])
-                first
-                :content
-                clean-html
-                clean-html
-                clean-html
-                ))))
+  (apply str (->
+              (select-from query [:div.s])
+              first
+              :content
+              clean-html
+              )))
+
+(defn convert [query]
+  (-> (select-from query [:h2.r])
+        first :content first :content first str))
+
+
 
 (defn search-command [irc channel message]
   (send-message irc channel
@@ -64,20 +71,16 @@ representation of text."
                      url-encode
                      get-info)))
 
-(defn make-url [address]
-    (if (.startsWith address "http")
-      (URL. address)
-      (URL. (str "http://" address))))
+
 
 
 
 (defn say-title [irc channel message]
-  (do (println  message)
-      (send-message irc channel
-                    (-> message
-                        first
-                        make-url
-                        get-title))))
+  (send-message irc channel
+                (-> message
+                    first
+                    make-url
+                    get-title)))
 
 (defn cleanup [string]
   (->
@@ -97,6 +100,9 @@ representation of text."
                                                  html-resource 
                                                  (select [:p])))))))))
 
+(defn say-convert [irc channel message]
+  (send-message irc channel (convert (second  message))))
+
 
 (def url-pattern  #"(http://[^ ]*)||(www.[^ ]*).*")
 
@@ -105,6 +111,7 @@ representation of text."
     url-pattern :>> (partial say-title irc channel)
     #"google:[ ]*(.*)" :>> (partial search-command irc channel)
     #"info:[ ]*(.*)" :>> (partial search-command-info irc channel)
+    #"c:[ ]*(.*)" :>> (partial say-convert irc channel)
     #"bokade.*" (say-booked irc channel)
     nil)
   (log-line message nick channel)
